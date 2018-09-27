bfc2d178fe3e18d923faf49ee485c0d43b194044
William Collins <punkhornsw@gmail.com>
2018-09-27 17:26:16 -0400

    [ENTESB-9529] Modified SAP type converters to show Exception on failed conversion

--- camel-sap/camel-sap-component/src/main/java/org/fusesource/camel/component/sap/converter/StructureConverter.java
+++ camel-sap/camel-sap-component/src/main/java/org/fusesource/camel/component/sap/converter/StructureConverter.java
@@ -20,13 +20,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import org.apache.camel.CamelException;
 import org.apache.camel.Converter;
 import org.eclipse.emf.ecore.EObject;
 import org.fusesource.camel.component.sap.model.rfc.Structure;
 import org.fusesource.camel.component.sap.model.rfc.impl.StructureImpl;
 import org.fusesource.camel.component.sap.util.Util;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * A Type Converter for SAP structure objects.
@@ -38,80 +37,75 @@ import org.slf4j.LoggerFactory;
 public enum StructureConverter {
 	INSTANCE;
 	
-	private static final Logger LOG = LoggerFactory.getLogger(StructureConverter.class);
-
-	@Converter(allowNull = true)
-	public static Structure toStructure(String string) {
+	@Converter()
+	public static Structure toStructure(String string) throws CamelException {
 		try {
 			EObject eObject = Util.unmarshal(string);
-			
+
 			if (StructureImpl.class.isInstance(eObject)) {
 				return (StructureImpl) eObject;
+			} else {
+				throw new CamelException("failed to convert String to Structure");
 			}
 		} catch (IOException e) {
-			// Ignore
-			LOG.warn("Failed to convert String to Structure", e);
-		} 
-		return null; 
+			throw new CamelException("failed to convert String to Structure: ", e);
+		}
 	}
 
-	@Converter(allowNull = true)
-	public static Structure toStructure(InputStream in) {
+	@Converter()
+	public static Structure toStructure(InputStream in) throws CamelException {
 		try {
 			EObject eObject = Util.fromInputStream(in);
-			
+
 			if (StructureImpl.class.isInstance(eObject)) {
 				return (StructureImpl) eObject;
+			} else {
+				throw new CamelException("failed to convert InputStream to Structure");
 			}
 		} catch (IOException e) {
-			// Ignore
-			LOG.warn("Failed to convert InputStream to Structure", e);
-		} 
-		return null; 
+			throw new CamelException("failed to convert InputStream to Structure", e);
+		}
 	}
 
-	@Converter(allowNull = true)
-	public static Structure toStructure(byte[] byteArray) {
+	@Converter()
+	public static Structure toStructure(byte[] byteArray) throws CamelException {
 		try {
 			EObject eObject = Util.unmarshal(new String(byteArray));
-			
+
 			if (StructureImpl.class.isInstance(eObject)) {
 				return (StructureImpl) eObject;
+			} else {
+				throw new CamelException("failed to convert byte array to Structure");
 			}
 		} catch (IOException e) {
-			// Ignore
-			LOG.warn("Failed to convert byte array to Structure", e);
-		} 
-		return null; 
+			throw new CamelException("failed to convert byte array to Structure", e);
+		}
 	}
 
-	@Converter(allowNull = true)
-	public static String toString(StructureImpl structure) {
+	@Converter()
+	public static String toString(StructureImpl structure) throws CamelException {
 		try {
 			return Util.marshal(structure);
 		} catch (IOException e) {
-			LOG.warn("Failed to convert Structure to String", e);
-			return null;
+			throw new CamelException("failed to convert Structure to String", e);
 		}
 	}
-	
-	@Converter(allowNull = true)
-	public static OutputStream toOutputStream(StructureImpl structure) {
+
+	@Converter()
+	public static OutputStream toOutputStream(StructureImpl structure) throws CamelException {
 		try {
 			return Util.toOutputStream(structure);
 		} catch (IOException e) {
-			LOG.warn("Failed to convert Structure to OutputStream", e);
-			return null;
+			throw new CamelException("failed to convert Structure to OutputStream", e);
 		}
 	}
-	
-	@Converter(allowNull = true)
-	public static InputStream toInputStream(StructureImpl structure) {
+
+	@Converter()
+	public static InputStream toInputStream(StructureImpl structure) throws CamelException {
 		try {
 			return Util.toInputStream(structure);
 		} catch (IOException e) {
-			LOG.warn("Failed to convert Structure to InputStream", e);
-			return null;
+			throw new CamelException("failed to convert Structure to InputStream", e);
 		}
 	}
 	
