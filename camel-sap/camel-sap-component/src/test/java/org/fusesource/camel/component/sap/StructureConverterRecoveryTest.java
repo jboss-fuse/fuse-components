4f34fef3326cea00f67048a6c002b6ecc3570c16
Amos Feng <zfeng@redhat.com>
2020-10-15 11:15:00 +0800

    fix the tests

--- camel-sap/camel-sap-component/src/test/java/org/fusesource/camel/component/sap/StructureConverterRecoveryTest.java
+++ camel-sap/camel-sap-component/src/test/java/org/fusesource/camel/component/sap/StructureConverterRecoveryTest.java
@@ -44,7 +44,6 @@ public class StructureConverterRecoveryTest extends SapRfcTestSupport {
 
 		PowerMockito.mockStatic(JCoDestinationManager.class, JCoServerFactory.class);
 		when(JCoDestinationManager.getDestination(DESTINATION_NAME)).thenReturn(mockDestination);
-		when(JCoServerFactory.get()).thenReturn(mockServerFactory);
 		when(JCoServerFactory.getServer(SERVER_NAME)).thenReturn(mockServer);
 	}
 
