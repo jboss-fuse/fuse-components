109bdc8068ab1d370534fc716cecbdb14b05ddf4
John Poth <poth.john@gmail.com>
2023-03-27 11:41:01 +0200

    Disable integration tests that should be run manually

--- camel-sap/camel-sap-component/src/test/java/org/fusesource/camel/component/sap/integration/ITestReceiveIDoc.java
+++ camel-sap/camel-sap-component/src/test/java/org/fusesource/camel/component/sap/integration/ITestReceiveIDoc.java
@@ -4,15 +4,17 @@ import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.test.spring.junit5.CamelSpringTestSupport;
 import org.fusesource.camel.component.sap.model.idoc.DocumentList;
 import org.fusesource.camel.component.sap.util.IDocUtil;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- * Integration test cases for receiving IDoc doucments
+ * Integration test cases for receiving IDoc documents
  * 
  * @author William Collins <punkhornsw@gmail.com>
  *
  */
+@Disabled("These tests are meant to be run manually (see infinite while loop in test method)")
 public class ITestReceiveIDoc extends CamelSpringTestSupport {
 
 	@Test
