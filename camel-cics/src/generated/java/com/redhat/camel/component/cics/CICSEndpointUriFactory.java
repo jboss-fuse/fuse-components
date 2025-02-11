aa8ca19012a63026c89b5006c647ba5c9900eb20
Tom Cunningham <tcunning@redhat.com>
2025-02-11 15:04:19 -0500

    Change versions to 4.10.0-SNAPSHOT

--- camel-cics/src/generated/java/com/redhat/camel/component/cics/CICSEndpointUriFactory.java
+++ camel-cics/src/generated/java/com/redhat/camel/component/cics/CICSEndpointUriFactory.java
@@ -23,7 +23,8 @@ public class CICSEndpointUriFactory extends org.apache.camel.support.component.E
     private static final Set<String> SECRET_PROPERTY_NAMES;
     private static final Set<String> MULTI_VALUE_PREFIXES;
     static {
-        Set<String> props = new HashSet<>(18);
+        Set<String> props = new HashSet<>(19);
+        props.add("callType");
         props.add("ctgDebug");
         props.add("dataExchangeType");
         props.add("eciBinding");
@@ -65,6 +66,7 @@ public class CICSEndpointUriFactory extends org.apache.camel.support.component.E
 
         uri = buildPathParameter(syntax, uri, "interfaceType", "eci", true, copy);
         uri = buildPathParameter(syntax, uri, "dataExchangeType", "commarea", false, copy);
+        uri = buildPathParameter(syntax, uri, "callType", 1, false, copy);
         uri = buildQueryParameters(uri, copy, encode);
         return uri;
     }
